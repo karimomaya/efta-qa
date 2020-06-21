a29f3a3bd4f0a117e061c97b47a8a2bbb2e85036
Mohamed Gamal <mohamed.gamal987.mg@gmail.com>
2020-06-21 18:19:34 +0200

    add method to map classification string and array to another value from json file

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 from excel import generate_excel
 from mongodb import connect_to_db, get_collection
+import json
+
+with open('config.json') as config_file:
+    config = json.load(config_file)
+
+with open('map-config.json') as map_config_file:
+    map_config = json.load(map_config_file)
+
+
+def update_classification():
+    for item in map_config['maps']:
+        query = {"classification": item['from']}
+        print(query)
+        updates = {"$set": {"classification": item['to'], 'classificationArr': item['to'].split(',')}}
+        print(updates)
+        # db = connect_to_db(config)
+        # collection = get_collection(db, "content")
+        # updated = collection.update_many(query, updates)
+        # print(updated.modified_count)
 
 
 def init_pip():
@@ -15,7 +34,7 @@ def init_pip():
 
 
 def export_excel():
-    db = connect_to_db()
+    db = connect_to_db(config)
     collection = get_collection(db, "content")
     docs = collection.aggregate(init_pip())
     generate_excel('data', docs)
@@ -23,4 +42,5 @@ def export_excel():
 
 
 if __name__ == '__main__':
-    export_excel()
+    # export_excel()
+    update_classification()
--- mongodb.py
+++ mongodb.py
@@ -1,13 +1,6 @@
 from pymongo import MongoClient
-import json
 
-
-
-with open('config.json') as json_data_file:
-    config = json.load(json_data_file)
-
-
-def connect_to_db():
+def connect_to_db(config):
     connection = MongoClient(config['mongo']['host'], config['mongo']['port'], unicode_decode_error_handler='ignore')
     db = connection[config['mongo']['database']]
     db.authenticate(config['mongo']['user'], config['mongo']['passwd'], 'admin')
